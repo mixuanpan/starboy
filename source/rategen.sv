ab2de9ca295a753d31c9daae2ccdedc778bbd648
Mixuan Pan <panmixuan@outlook.com>
2025-07-07 16:16:27 -0400

    original tetris fsm

--- source/rategen.sv
+++ source/rategen.sv
@@ -1,65 +1,65 @@
-'timescale 1ns / 1ps
+// `timescale 1ns / 1ps
 
-/////////////////////////////////////////////////////////////////
-// HEADER 
-//
-// Module : rategen
-// Description : Manages speed of falling blocks 
-// 
-//
-/////////////////////////////////////////////////////////////////
+// /////////////////////////////////////////////////////////////////
+// // HEADER 
+// //
+// // Module : rategen
+// // Description : Manages speed of falling blocks 
+// // 
+// //
+// /////////////////////////////////////////////////////////////////
 
-module rategen#( 
-parameter int unsigned CLK_DIV_GAMESPEED_1 = 50000000 - 1;   
-parameter int unsigned CLK_DIV_GAMESPEED_2 = 4500000 - 1; 
-parameter int unsigned CLK_DIV_GAMESPEED_3 = 4000000 - 1; 
-parameter int unsigned CLK_DIV_GAMESPEED_4 = 3500000 - 1; 
-parameter int unsigned CLK_DIV_GAMESPEED_5 = 3000000 - 1; 
-parameter int unsigned CLK_DIV_GAMESPEED_6 = 2500000 - 1; 
-parameter int unsigned CLK_DIV_GAMESPEED_7 = 2000000 - 1; 
-parameter int unsigned CLK_DIV_GAMESPEED_8 = 15000000 - 1;  
-parameter int unsigned CLK_DIV_GAMESPEED_9 = 1000000 - 1; 
-parameter int unsigned CLK_DIV_DROP = 5000000 - 1
-)
-    (
-    input clk,
-    input reset,
-    input drop,
-    input [3:0] speed,
-    output en
-);
+// module rategen#( 
+// parameter int unsigned CLK_DIV_GAMESPEED_1 = 50000000 - 1;   
+// parameter int unsigned CLK_DIV_GAMESPEED_2 = 4500000 - 1; 
+// parameter int unsigned CLK_DIV_GAMESPEED_3 = 4000000 - 1; 
+// parameter int unsigned CLK_DIV_GAMESPEED_4 = 3500000 - 1; 
+// parameter int unsigned CLK_DIV_GAMESPEED_5 = 3000000 - 1; 
+// parameter int unsigned CLK_DIV_GAMESPEED_6 = 2500000 - 1; 
+// parameter int unsigned CLK_DIV_GAMESPEED_7 = 2000000 - 1; 
+// parameter int unsigned CLK_DIV_GAMESPEED_8 = 15000000 - 1;  
+// parameter int unsigned CLK_DIV_GAMESPEED_9 = 1000000 - 1; 
+// parameter int unsigned CLK_DIV_DROP = 5000000 - 1
+// )
+//     (
+//     input clk,
+//     input reset,
+//     input drop,
+//     input [3:0] speed,
+//     output en
+// );
 
-logic [25:0] compare;
+// logic [25:0] compare;
 
-always_comb begin
-    case (speed)
-    4'd1: compare = CLK_DIV_GAMESPEED_1;
-    4'd2: compare = CLK_DIV_GAMESPEED_2;
-    4'd3: compare = CLK_DIV_GAMESPEED_3;
-    4'd4: compare = CLK_DIV_GAMESPEED_4;
-    4'd5: compare = CLK_DIV_GAMESPEED_5;
-    4'd6: compare = CLK_DIV_GAMESPEED_6;
-    4'd7: compare = CLK_DIV_GAMESPEED_7;
-    4'd8: compare = CLK_DIV_GAMESPEED_8;
-    4'd9: compare = CLK_DIV_GAMESPEED_9;
+// always_comb begin
+//     case (speed)
+//     4'd1: compare = CLK_DIV_GAMESPEED_1;
+//     4'd2: compare = CLK_DIV_GAMESPEED_2;
+//     4'd3: compare = CLK_DIV_GAMESPEED_3;
+//     4'd4: compare = CLK_DIV_GAMESPEED_4;
+//     4'd5: compare = CLK_DIV_GAMESPEED_5;
+//     4'd6: compare = CLK_DIV_GAMESPEED_6;
+//     4'd7: compare = CLK_DIV_GAMESPEED_7;
+//     4'd8: compare = CLK_DIV_GAMESPEED_8;
+//     4'd9: compare = CLK_DIV_GAMESPEED_9;
     
-    default: compare = CLK_DIV_GAMESPEED_1;
-    endcase 
-end
+//     default: compare = CLK_DIV_GAMESPEED_1;
+//     endcase 
+// end
 
 
-logic [25:0] cntr;
-always @ (posedge clk) begin 
-    if (rst) begin
-        cntr <= '0;
-    end else ((!drop && cntr >= compare) || (drop && cntr >= CLK_DIV_DROP)) begin
-        cntr <= '0;
-    end
-    else begin
-        cntr <= cntr + 1'b1'
-    end
-end
+// logic [25:0] cntr;
+// always @ (posedge clk) begin 
+//     if (rst) begin
+//         cntr <= '0;
+//     end else ((!drop && cntr >= compare) || (drop && cntr >= CLK_DIV_DROP)) begin
+//         cntr <= '0;
+//     end
+//     else begin
+//         cntr <= cntr + 1'b1'
+//     end
+// end
 
-assign en = drop ? (cntr == CLK_DIV_DROP) : (cntr == compare);
+// assign en = drop ? (cntr == CLK_DIV_DROP) : (cntr == compare);
 
-endmodule
+// endmodule
--- testbench/tetris_tb.sv
+++ testbench/tetris_tb.sv
@@ -1,14 +1,43 @@
 `timescale 1ms/10ps
+typedef enum logic [4:0] {
+  IDLE, // reset state 
+  READY, // count down to start 
+  NEW_BLOCK, // load new block 
+  A1, 
+  A2, 
+  B1, 
+  B2, 
+  C1, 
+  C2, 
+  D, 
+  E1, 
+  E2, 
+  E3, 
+  E4, 
+  F1, 
+  F2, 
+  F3, 
+  F4, 
+  G1, 
+  G2, 
+  G3, 
+  G4, 
+  EVAL, // evaluation 
+  GAMEOVER // user run out of space 
+} state_t; 
+
 module tetris_tb;
   logic clk, rst, en, right, left, rr, rl; 
-  logic [9:0] grid [21:0]; 
-  tetris game (.clk(clk), .rst(rst), .en(en), .right(right), .left(left), .down(), .rr(rr), .rl(rl), .count_down(), .grid(grid));
+  logic grid [21:0][9:0]; 
+  logic [9:0] row_temp; 
+  logic [4:0] state; 
+  tetris game (.clk(clk), .rst(rst), .en(en), .right(right), .left(left), .down(), .rr(rr), .rl(rl), .count_down(), .state_o(state));
   
   initial clk = 0; 
   always clk = #1 ~clk; 
 
   task toggle_rst();
-    rst = 1; #1; 
+    rst = 1; #2; 
     rst = 0; 
   endtask 
   
@@ -16,19 +45,49 @@ module tetris_tb;
     // make sure to dump the signals so we can see them in the waveform
     $dumpfile("waves/tetris.vcd"); //change the vcd vile name to your source file name
     $dumpvars(0, tetris_tb);
-    // for loop to test all possible inputs
+    rst = 0; 
+    en = 1'b0; 
+    #2; 
+    toggle_rst(); 
+    // en = 1'b1; 
+    #2; 
+    // $display("Grid00=\%b", grid[0][0]);
+
     for (integer i = 0; i <= 1; i++) begin
       for (integer j = 0; j <= 1; j++) begin
         for (integer k = 0; k <= 1; k++) begin
-        // set our input signals
-        A = i; B = j; Cin = k;
-        #1;
-        // display inputs and outputs
-        $display("A=\%b, B=\%b, Cin=\%b, Cout=\%b, S=\%b", A, B, Cin, Cout, S);
+          for (integer l = 0; l <= 1; l++) begin
+            for (integer m = 0; m <= 1; m++) begin
+              // set our input signals
+              // en = i[0]; 
+              right = j[0]; left = k[0]; rr = l[0]; rl = m[0]; 
+              #1;
+              // display inputs and outputs
+              //  right = 1; 
+              // left = 1; 
+              // rr = 1;
+              // rl = 1; 
+
+              // for (int i = 0; i < 22; i++) begin
+              // for (int j = 0; j < 10; j++)
+              //     row_temp[j] = grid[i][j];
+              // $display("Row %0d: %b", i, row_temp);
+                $display("Grid after 100ns:");
+                for (int i = 0; i < 22; i++) begin
+                  for (int j = 0; j < 10; j++) $write("%0d", grid[i][j]);
+                  $write("\n");
+                end
+              $display("en=\%b, right=\%b, left=\%b, rr=\%b, rl=\%b, state=\%b, GRID00=\%b", en, right, left, rr, rl, state, grid[0][0]);
+            end
+          end
         end
       end
     end
+
+
+ 
+// end
   // finish the simulation
-  #1 $finish;
+  #1; $finish;
   end
 endmodule
