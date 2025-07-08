36d4d46bfa76457af2b9f0af708c448461eb8e08
Myles <mquerimi@purdue.edu>
2025-07-08 15:49:03 -0400

    inputbus module

---
+++ source/inputbus.sv
@@ -0,0 +1,18 @@
+`default_nettype none
+
+/////////////////////////////////////////////////////////////////
+// HEADER 
+//
+// Module : inputbus
+// Description : converts the pb inputs to moves
+// 
+//
+/////////////////////////////////////////////////////////////////
+
+module inputbus (
+  input logic [3:0] in, 
+  input logic enable, 
+  output logic [7:0] out
+);
+
+endmodule
