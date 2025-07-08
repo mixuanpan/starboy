9990af69d1fcc52ecf38addc65283f36cd342698
Myles <mquerimi@purdue.edu>
2025-07-08 16:02:45 -0400

    testing

--- source/inputbus.sv
+++ source/inputbus.sv
@@ -15,4 +15,23 @@ module inputbus (
   output logic [7:0] out
 );
 
+
+logic [4:0] btn_prev;
+
+always_ff @(posedge clk, negedge rst_n) begin
+    if (!rstn_n) begin
+        btn_prev <= 5'd0;
+    end else begin
+        btn_prev <= btn_raw;
+    end
+end
+
+logic [4:0] btn_edge;
+assign btn_edge = btn_raw & ~btn_prev;
+
+always_comb begin
+    move_valid = 1'b0;
+    move = DOWN; 
+    if 
+
 endmodule
