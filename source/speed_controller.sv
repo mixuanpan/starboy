97a4ece7802679284bbadd8f62cd8733b0bb7918
mart2667 <mart2667@purdue.edu>
2025-07-23 09:30:30 -0400

    test

--- source/speed_controller.sv
+++ source/speed_controller.sv
@@ -1,49 +1,39 @@
 `default_nettype none
-
 module speed_controller (
     input logic clk,
     input logic reset,
     input logic [7:0] current_score,
     output logic [24:0] scoremod
 );
-
     // Internal signals
     logic [7:0] prev_score;
-    logic [4:0] increase;
-    logic speed_increased, next_speed_increased;
     logic [24:0] next_mod;
-    logic [4:0] newval;
-
+    
     // Logic for increased timing as game progresses
     always_ff @(posedge clk, posedge reset) begin
         if (reset) begin
             scoremod <= '0;
-            increase <= 1;
             prev_score <= '0;
-            speed_increased <= 1'b0;
         end else begin
             scoremod <= next_mod;
             prev_score <= current_score;
-            speed_increased <= next_speed_increased;
         end
     end
-
+    
     always_comb begin
+        logic [7:0] prev_threshold, curr_threshold;
+        logic [24:0] speed_increases;
+        
         next_mod = scoremod;
-        newval = increase;
-        next_speed_increased = speed_increased;
         
-        // Reset flag when score changes but isn't at a multiple of 5
-        if (current_score != prev_score && current_score % 5 != 0) begin
-            next_speed_increased = 1'b0;
-        end
+        // Calculate how many multiples of 5 each score represents
+        prev_threshold = prev_score / 5;
+        curr_threshold = current_score / 5;
         
-        // Increase speed when we hit a multiple of 5 and haven't already increased
-        if (current_score != prev_score && current_score % 5 == 0 && 
-            current_score != '0 && !speed_increased) begin
-            next_mod = scoremod + 25'd1_000_000;
-            next_speed_increased = 1'b1;
+        // If we've crossed one or more thresholds, add the appropriate speed increases
+        if (curr_threshold > prev_threshold) begin
+            speed_increases = (curr_threshold - prev_threshold) * 25'd1_000_000;
+            next_mod = scoremod + speed_increases;
         end
     end
-
 endmodule
