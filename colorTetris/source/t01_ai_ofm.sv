2dd972a3fa6a0b5f8a66aeb40642a60582cffbff
Mixuan Pan <pan453@purdue.edu>
2025-08-02 15:53:15 -0400

    ofm + tb done

--- colorTetris/source/t01_ai_ofm.sv
+++ colorTetris/source/t01_ai_ofm.sv
@@ -1,35 +1,46 @@
 `default_nettype none 
 module t01_ai_ofm (
     input logic clk, rst, mmu_done,
-    input logic [17:0] n_mmu_result,
-    input logic [3:0] n_blockX, 
-    input logic [4:0] n_block_type, 
-    output logic [3:0] blockX, 
-    output logic [4:0] block_type,  
+    input logic [17:0] mmu_result_i,
+    input logic [3:0] blockX_i, 
+    input logic [4:0] block_type_i, 
+    output logic [3:0] blockX_o, 
+    output logic [4:0] block_type_o,  
     output logic done
 ); 
-    logic [4:0] c_block_type;
-    logic [3:0] c_blockX;  
-    logic [17:0] c_mmu_result; 
+    logic [4:0] c_block_type, n_block_type;
+    logic [3:0] c_blockX, n_blockX;  
+    logic [17:0] c_mmu_result, n_mmu_result; 
     logic testing; 
-    assign testing = n_mmu_result > c_mmu_result; 
+    assign testing = mmu_result_i > c_mmu_result; 
 
-    // assign block_type = c_block_type; 
-    // assign blockX = c_blockX; 
-
+    assign blockX_o = c_blockX; 
+    assign block_type_o = c_block_type; 
     always_ff @(posedge clk, posedge rst) begin 
         if (rst) begin 
             c_mmu_result <= 0; 
-            block_type <= 0;
-            blockX <= 0;
-            done <= 0;  
+            c_block_type <= 0;
+            c_blockX <= 0; 
+            done <= 0; 
         end else if (mmu_done) begin 
-            if (n_mmu_result > c_mmu_result) begin 
-                c_mmu_result <= n_mmu_result; 
-                blockX <= n_blockX; 
-                block_type <= n_block_type; 
-            end
+            c_blockX <= n_blockX; 
+            c_block_type <= n_block_type; 
+            c_mmu_result <= n_mmu_result; 
             done <= 1'b1; 
+        end else begin 
+            done <= 0; 
         end
     end 
+
+    always_comb begin 
+        n_mmu_result = c_mmu_result; 
+        n_blockX = c_blockX; 
+        n_block_type = c_block_type; 
+
+        if (mmu_result_i > c_mmu_result) begin 
+            n_blockX = blockX_i; 
+            n_block_type = block_type_i; 
+            n_mmu_result = mmu_result_i; 
+        end
+    end
 endmodule 
--- colorTetris/testbench/t01_ai_ofm_tb.sv
+++ colorTetris/testbench/t01_ai_ofm_tb.sv
@@ -2,15 +2,15 @@
 module t01_ai_ofm_tb; 
     logic clk, rst;
     logic mmu_done; 
-    logic [17:0] n_mmu_result; 
-    logic [3:0] n_blockX; 
-    logic [4:0] n_block_type; 
-    logic [3:0] blockX; 
-    logic [4:0] block_type; 
+    logic [17:0] mmu_result_i; 
+    logic [3:0] blockX_i; 
+    logic [4:0] block_type_i; 
+    logic [3:0] blockX_o; 
+    logic [4:0] block_type_o; 
     logic done; 
      
    
-    t01_ai_ofm ge (.clk(clk), .rst(rst), .mmu_done(mmu_done), .n_mmu_result(n_mmu_result), .n_blockX(n_blockX), .n_block_type(n_block_type), .blockX(blockX), .block_type(block_type), .done(done)); 
+    t01_ai_ofm ge (.clk(clk), .rst(rst), .mmu_done(mmu_done), .mmu_result_i(mmu_result_i), .blockX_i(blockX_i), .block_type_i(block_type_i), .blockX_o(blockX_o), .block_type_o(block_type_o), .done(done)); 
 
     task tog_rst; 
         rst = 1; #1; 
@@ -25,20 +25,21 @@ module t01_ai_ofm_tb;
         $dumpfile("waves/t01_ai_ofm.vcd"); 
         $dumpvars(0, t01_ai_ofm_tb);
 
-        n_blockX = 'd7; 
-        n_block_type = 'd3; 
+        blockX_i = 'd7; 
+        block_type_i = 'd3; 
         mmu_done = 1'b1; 
         tog_rst(); 
-            for (int i = 0; i <= 'd10; i++) begin 
-                for (int j = 0; j <= 'd7; j++) begin 
-                    for (int k = 0; k <= 'd9; k++) begin 
+            
+                for (int j = 3; j <= 'd7; j++) begin 
+                    for (int k = 2; k <= 'd9; k++) begin 
+                        for (int i = 0; i <= 'd10; i++) begin 
                         if (i == 'd5) begin 
-                            n_mmu_result = 'd0; 
+                            mmu_result_i = 'd0; 
                         end else begin 
-                            n_mmu_result = i[17:0];
+                            mmu_result_i = i[17:0];
                         end  
-                        n_block_type = j[4:0]; 
-                        n_blockX = k[3:0]; 
+                        block_type_i = j[4:0]; 
+                        blockX_i = k[3:0]; 
                         #1; 
                     end
                 end
