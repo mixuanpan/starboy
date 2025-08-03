656f03a52685e6cccfffb57f9451f14de53fa967
Mixuan Pan <panmixuan@gmail.com>
2025-08-03 12:39:58 -0400

    rewrote feature extract

---
+++ colorTetris/source/t01_ai_feature_extract_new.sv
@@ -0,0 +1,164 @@
+`default_nettype none
+/////////////////////////////////////////////////////////////////
+// HEADER 
+//
+// Module : feature_extractor
+// Description : extracts features from the Tetris board state
+// 
+//
+/////////////////////////////////////////////////////////////////
+module t01_ai_feature_extract_new (
+    input logic clk,
+    input logic rst,
+    input logic extract_start,
+    input logic [19:0][9:0] tetris_grid, 
+    
+    output logic extract_ready,
+    output logic [7:0] lines_cleared,
+    output logic [7:0] holes,
+    output logic [7:0] bumpiness,  
+    output logic [7:0] height_sum, 
+
+    // for testing
+    output logic [2:0] state 
+);
+    // fsm state transition 
+    typedef enum logic [2:0] {
+        IDLE, 
+        LINES, 
+        OTHER, // for loop buffer 
+        HEIGHT, // heights & bumpiness 
+        HOLES, 
+        DONE 
+    } extract_state_t; 
+
+    extract_state_t c_state, n_state; 
+    assign state = c_state; 
+
+    // line clear 
+    logic [3:0] gamestate; // gamestate simulator 
+    logic [19:0][9:0] cleared_array; // array after lines cleared 
+    logic clear_complete; 
+    
+    t01_lineclear line_clear_master (
+        .clk(clk), 
+        .reset(rst), 
+        .gamestate(gamestate), 
+        .start_eval(1'b1), 
+        .input_array(tetris_grid), 
+        .input_color_array(), 
+        .output_array(cleared_array), 
+        .eval_complete(clear_complete), 
+        .score(lines_cleared)
+    );
+
+    // heights 
+    logic [4:0] heights [0:9]; 
+    logic [3:0] height_column_counter, n_height_column_counter; 
+    assign height_sum = heights[0] + heights[1] + heights[2] + heights[3] + heights[4] + heights[5] + heights[6] + heights[7] + heights[8] + heights[9]; 
+    
+    // bumpiness 
+    logic [4:0] bump_spread [0:8]; 
+    assign bump_spread[0] = heights[0] > heights[1] ? heights[0] - heights[1] : heights[1] - heights[0]; 
+    assign bump_spread[1] = heights[1] > heights[2] ? heights[1] - heights[2] : heights[2] - heights[1]; 
+    assign bump_spread[2] = heights[2] > heights[3] ? heights[2] - heights[3] : heights[3] - heights[2]; 
+    assign bump_spread[3] = heights[3] > heights[4] ? heights[3] - heights[4] : heights[4] - heights[3]; 
+    assign bump_spread[4] = heights[4] > heights[5] ? heights[4] - heights[5] : heights[5] - heights[4]; 
+    assign bump_spread[5] = heights[5] > heights[6] ? heights[5] - heights[6] : heights[6] - heights[5]; 
+    assign bump_spread[6] = heights[6] > heights[7] ? heights[6] - heights[7] : heights[7] - heights[6]; 
+    assign bump_spread[7] = heights[7] > heights[8] ? heights[7] - heights[8] : heights[8] - heights[7]; 
+    assign bumpiness = bump_spread[0] + bump_spread[1] + bump_spread[2] + bump_spread[3] + bump_spread[4] + bump_spread[5] + bump_spread[6] + bump_spread[7] + bump_spread[8]; 
+    
+    // holes 
+    logic [3:0] hole_column_counter, n_hole_column_counter; 
+    logic hole_perceived, n_hole_perceived; 
+    logic [7:0] c_holes, n_holes, c_hole_start_row, n_hole_start_row; 
+    assign holes = c_holes; 
+
+    always_ff @(posedge clk, posedge rst) begin 
+        if (rst) begin 
+            c_state <= IDLE; 
+            c_holes <= 0; 
+            height_column_counter <= 0; 
+            hole_column_counter <= 0; 
+            hole_perceived <= 0; 
+            c_hole_start_row <= 'd18; 
+        end else if (extract_start) begin 
+            c_state <= n_state; 
+            c_holes <= n_holes; 
+            height_column_counter <= n_height_column_counter; 
+            hole_column_counter <= n_hole_column_counter; 
+            hole_perceived <= n_hole_perceived; 
+            c_hole_start_row <= n_hole_start_row; 
+        end
+    end
+
+    always_comb begin 
+        n_state = c_state; 
+        n_holes = c_holes; 
+        gaemstate = 'd9; 
+        extract_ready = 0; 
+        n_height_column_counter = height_column_counter; 
+        n_hole_column_counter = hole_column_counter; 
+        n_hole_perceived = hole_perceived; 
+        n_hole_start_row = c_hole_start_row; 
+
+        case (c_state) 
+            IDLE: begin 
+                if (extract_start) begin 
+                    gamestate = 'd9; 
+                    n_state = LINES; 
+                end 
+            end
+            LINES: begin 
+                gamestate = 'd10; 
+                n_height_column_counter = 0; 
+                if (clear_complete) begin 
+                    n_state = OTHER; 
+                end
+            end
+            OTHER: begin 
+                if (hole_column_counter >= 'd10) begin 
+                    n_state = DONE; 
+                end else if (height_column_counter >= 'd10) begin 
+                    for (int r = c_hole_start_row; r >= 'd18 - heights[hole_column_counter]; r++) begin 
+                        if (cleared_array[r-1][hole_column_counter] && !cleared_array[r][hole_column_counter] && cleared_array[r+1][hole_column_counter]) begin 
+                            n_hole_start_row = r + 'd2; 
+                            n_hole_perceived = 1; 
+                            n_state = HOLES; 
+                        end
+                    end
+                    n_state = HOLES; 
+                end else begin 
+                    for (int r = 19; r >= 0; r--) begin 
+                        if (|cleared_array[r][height_column_counter]) begin 
+                            heights[height_column_counter] = 'd20 - r[7:0]; 
+                        end
+                    end
+                    n_state = HEIGHT; 
+                end 
+            end
+            HEIGHT: begin 
+                n_height_column_counter = height_column_counter + 1; 
+                n_state = OTHER; 
+            end
+            HOLES: begin 
+                if (hole_perceived) begin 
+                    n_holes = c_holes + 1; 
+                end
+                if (c_hole_start_row >= 'd18 - heights[hole_column_counter]) begin 
+                    n_hole_start_row = 0; 
+                    n_hole_column_counter = hole_column_counter + 1;  
+                end
+                n_hole_perceived = 0;
+                n_state = OTHER;
+            end
+            DONE: begin 
+                extract_ready = 1; 
+                if (!extract_start) begin 
+                    n_state = IDLE; 
+                end
+            end
+        endcase
+    end
+endmodule 
